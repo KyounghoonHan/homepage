0c96f64a082fbb82254429166cc50b948b218410
khhan <khhan7813@gmail.com>
2022-01-25 12:52:52 +0900

    Create blog list pages by using CBV

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'), 
+    path('', views.PostList.as_view()),
+    
     path('<int:pk>/', views.single_page_post, name='single_page_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from .models import Post
 
+from django.views.generic import ListView
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    context = {'posts': posts}
+
+
+class PostList(ListView):
+    """ PostList by CBV """
+    model = Post
+    template_name = 'blog/index.html' # set a template manually
+    ordering = '-pk'
     
-    return render(
-        request,
-        'blog/index.html',
-        context
-    )
+
+# def index(request):
+    """ index by FBV """
+#     posts = Post.objects.all().order_by('-pk')
+#     context = {'posts': posts}
+    
+#     return render(
+#         request,
+#         'blog/index.html',
+#         context
+#     )
     
 def single_page_post(request, pk):
     post = Post.objects.get(pk=pk)
