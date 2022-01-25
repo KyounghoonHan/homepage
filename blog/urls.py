b47f5d05e1e7ecb0e0de36ca4328a064dc5e814a
khhan <khhan7813@gmail.com>
2022-01-25 11:21:38 +0900

    Create blog detail page by using FBV

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pk>/', views.single_page_post, name='single_page_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,19 @@ from .models import Post
 def index(request):
     posts = Post.objects.all().order_by('-pk')
     context = {'posts': posts}
+    
     return render(
         request,
         'blog/index.html',
+        context
+    )
+    
+def single_page_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {'post': post}
+    
+    return render(
+        request,
+        'blog/single_page.html',
         context
     )
