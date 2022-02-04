dd855fc2e81d1280f1375488c1e49e83ab03c0f8
khhan <khhan7813@gmail.com>
2022-02-04 15:24:29 +0900

    Adapted django-crispy-forms to post forms

--- webpage/settings.py
+++ webpage/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    # installed manually
+    # installed apps manually
     'blog',
     'single_page',
+    
+    # installed libraries manually
     'django_extensions',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
