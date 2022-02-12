3b86280e91c8b4317e1c0caf2180449c812cce6f
khhan <khhan7813@gmail.com>
2022-02-12 14:02:44 +0900

    Added landing page & portfolio page

--- single_page/tests.py
+++ single_page/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from blog.models import Post
+from bs4 import BeautifulSoup
 
 # Create your tests here.
+
+class TestVeiw(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_trump = User.objects.create_user(
+            username='trump',
+            password='somepassword'
+        )
+        
+    def test_landing(self):
+        for i in range(1, 5):
+            globals()['post_00{}'.format(i)] = Post.objects.create(
+                title=f'{i} post',
+                content=f'{i} post',
+                author=self.user_trump
+            )
+
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        body = soup.body
+        self.assertNotIn(post_001.title, body.text)
+        self.assertIn(post_002.title, body.text)
+        self.assertIn(post_003.title, body.text)
+        self.assertIn(post_004.title, body.text)
+        
--- single_page/views.py
+++ single_page/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    context = {'recent_posts': recent_posts}
+    
     return render(
         request,
-        'single_page/landing.html'
+        'single_page/landing.html',
+        context
     )
     
 def about_me(request):
