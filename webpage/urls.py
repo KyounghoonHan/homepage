d0d04071a037f61b42ef2e5da201b5a151295dd7
khhan <khhan7813@gmail.com>
2022-02-17 16:52:53 +0900

    Setup gunicorn as WSGI

--- webpage/urls.py
+++ webpage/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path('', include('single_page.urls')),
     path('markdownx/', include('markdownx.urls')),
     path('accounts/', include('allauth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
